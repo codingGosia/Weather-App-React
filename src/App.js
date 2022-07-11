541ed9f1b38dc6cea13dd63abcff6647df58e4e0
Gosia <77539154+codingGosia@users.noreply.github.com>
2022-07-11 14:24:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
+        <Weather defaultCity="Stockholm" />
         <footer>
           This project is coded by Gosia and is open-sourced on{" "}
           <a
