434f9468a7c90cf17e229cf98980f899b12c89ac
Gosia <77539154+codingGosia@users.noreply.github.com>
2022-07-11 13:43:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Stockholm" />
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            gosiaCoding
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/gosiaCoding"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced
+          </a>
+          {""} on Github and hosted on{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/WeatherForecast.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [forecastReady, setForecastReady] = useState(false);
+  let [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    setForecastReady(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecastData(response.data.daily);
+    setForecastReady(true);
+  }
+
+  if (forecastReady) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "a2c1979c00399a5761263bb164c05266";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
